454c6d136cdfd671ac1fb4c3fcc0bd477a1966b0
ElTinMar <martin.privat@hotmail.fr>
2023-04-21 13:22:07 +0200

    renaming

--- utils/frame_converter.py
+++
@@ -1,9 +0,0 @@
-import typing 
-from numpy.typing import NDArray
-import numpy as np
-
-def frame_converter(frame: NDArray) -> NDArray[np.single]:
-    """
-    Convert an image from whatever bit depth to single precision
-    """
-    pass
