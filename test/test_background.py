d6c8ae98e5cbbbbf0775875d49de650df4366280
ElTinMar <martin.privat@hotmail.fr>
2023-04-25 18:02:10 +0200

    adding area opening

--- test/test_background.py
+++ test/test_background.py
@@ -3,8 +3,10 @@ import utils.background_model as bckg
 import cv2
 from utils.im2float import im2single
 from utils.im2gray import im2gray
+from utils.imadjust import imadjust
+from skimage.morphology import area_opening
 
-mov = OpenCV_VideoReader('toy_data/Rajan_et_al_2022.mp4',safe=False)
+mov = OpenCV_VideoReader('toy_data/behavior_2000.avi',safe=False)
 sample = bckg.sample_frames_evenly(mov,500)
 background = bckg.background_model_mode(sample)
 
@@ -19,9 +21,12 @@ while True:
     rval, frame = mov.next_frame()
     if not rval:
         break
-    bckg_sub = (im2single(im2gray(frame)) - background)**2
-    bckg_sub_norm = (bckg_sub - bckg_sub.min()) / (bckg_sub.max() - bckg_sub.min())
-    cv2.imshow('Background Subtraction', bckg_sub_norm)
+    bckg_sub = abs(im2single(im2gray(frame)) - background)
+    bckg_sub_norm = imadjust(bckg_sub,bckg_sub.min(),bckg_sub.max(),0,1)
+
+    # this is very good but slow
+    fish = area_opening(bckg_sub_norm,area_threshold=200)
+    cv2.imshow('Background Subtraction', fish)
     
     key = cv2.waitKey(1)
     if key == ord('q'):
