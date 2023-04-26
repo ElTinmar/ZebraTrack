5e8e128ca7bd49ce9067cb8684a3ada6ebaf51bb
ElTinMar <martin.privat@hotmail.fr>
2023-04-26 18:23:45 +0200

    implementation in progress

--- eyes/eyes_tracker.py
+++ eyes/eyes_tracker.py
@@ -3,13 +3,15 @@ import numpy as np
 from numpy.typing import NDArray
 import cv2
 from utils.conncomp_filter import bwareafilter
+from skimage.measure import label, regionprops
 
 def eyes_tracker(
         frame: NDArray,
         threshold_eye_intensity: float,
         threshold_eye_area_min: int,
         threshold_eye_area_max: int,
-        principal_components: NDArray
+        principal_components: NDArray,
+        fish_centroid: NDArray
         ) -> Tuple[float, float]:
     """
     Track the left and right eyes for a single fish
@@ -26,4 +28,11 @@ def eyes_tracker(
         max_size = threshold_eye_area_max
     )
 
-    pass
+    label_img = label(eye_mask)
+    regions = regionprops(label_img)
+
+    eye_left = None
+    for blob in regions:
+        # project coordinates to principal component space
+        coord_pc = (blob.centroid - fish_centroid) @ principal_components
+
