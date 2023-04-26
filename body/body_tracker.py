69f5fbbb59c35d31ec3d857078fcc89ecd366476
ElTinMar <martin.privat@hotmail.fr>
2023-04-26 14:48:32 +0200

    implemented body tracker

--- body/body_tracker.py
+++ body/body_tracker.py
@@ -2,8 +2,11 @@ from typing import Tuple
 import numpy as np
 from numpy.typing import NDArray
 import cv2
+from sklearn.decomposition import PCA
+from utils.conncomp_filter import bwareaopen
 
-def body_tracker(frame, method) -> Tuple(NDArray,NDArray):
+
+def body_tracker(frame, method) -> Tuple[NDArray,NDArray]:
     """
     Track the centroid and anteroposterior axis for a single fish
     Input: 
@@ -18,10 +21,39 @@ def body_tracker(frame, method) -> Tuple(NDArray,NDArray):
 
     pass
 
+def body_tracker_PCA(
+        frame: NDArray, 
+        threshold_intensity: float, 
+        threshold_area: int
+        ) -> Tuple[NDArray,NDArray,NDArray]:
+    """
+    Frame should be single or double precision with values between 0 and 1.
+    """
 
-def body_tracker_PCA(frame) -> Tuple(NDArray,NDArray):
-    
-    pass
+    # threshold and remove small objects 
+    fish_mask = bwareaopen(frame >= threshold_intensity, min_size=threshold_area)
+    blob_coordinates = np.argwhere(fish_mask)
 
-def body_tracker_moments(frame) -> Tuple(NDArray,NDArray):
+    if blob_coordinates.size == 0:
+        # nothing was detected above threshold
+        return (None, None, None)
+    else:
+        # match x,y and image coordinates
+        blob_coordinates = blob_coordinates[:,[1, 0]]
+
+        # PCA
+        pca = PCA()
+        scores = pca.fit_transform(blob_coordinates)
+        components = pca.components_.T
+        heading = components[:,0]
+        centroid = pca.mean_
+
+        # correct orientation
+        if abs(max(scores[:,0])) > abs(min(scores[:,0])):
+            heading = -heading
+
+        return (centroid, heading, fish_mask)
+      
+
+def body_tracker_moments(frame: NDArray) -> Tuple[NDArray,NDArray]:
     pass
