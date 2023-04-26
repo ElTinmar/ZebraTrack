dbb5fa8731980422335081e788caec959e90fa17
ElTinMar <martin.privat@hotmail.fr>
2023-04-26 14:49:04 +0200

    implementing file

--- eyes/eyes_tracker.py
+++ eyes/eyes_tracker.py
@@ -3,7 +3,11 @@ import numpy as np
 from numpy.typing import NDArray
 import cv2
 
-def eyes_tracker(frame) -> Tuple(float, float):
+def eyes_tracker(
+        frame: NDArray,
+        threshold_eye_intensity: float,
+        threshold_eye_area: int,
+        ) -> Tuple[float, float]:
     """
     Track the left and right eyes for a single fish
     Input: 
@@ -12,4 +16,5 @@ def eyes_tracker(frame) -> Tuple(float, float):
         (left,right): angle in radians for the left and right eye
         with respect to the anteroposterior axis 
     """
+    
     pass
