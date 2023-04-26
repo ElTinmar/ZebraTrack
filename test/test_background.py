ca49a7c230fb149adb8321a2fe3218ee27b892cf
ElTinMar <martin.privat@hotmail.fr>
2023-04-26 10:08:36 +0200

    adding imports

--- test/test_background.py
+++ test/test_background.py
@@ -5,6 +5,8 @@ from utils.im2float import im2single
 from utils.im2gray import im2gray
 from utils.imadjust import imadjust
 from skimage.morphology import area_opening
+from skimage.measure import regionprops, moments
+from scipy.ndimage import median_filter
 
 mov = OpenCV_VideoReader('toy_data/behavior_2000.avi',safe=False)
 sample = bckg.sample_frames_evenly(mov,500)
@@ -23,9 +25,10 @@ while True:
         break
     bckg_sub = abs(im2single(im2gray(frame)) - background)
     bckg_sub_norm = imadjust(bckg_sub,bckg_sub.min(),bckg_sub.max(),0,1)
+    #bckg_sub_norm_filt = median_filter(bckg_sub_norm,size=(5,5))
 
     # this is very good but slow
-    fish = area_opening(bckg_sub_norm,area_threshold=200)
+    fish = area_opening(bckg_sub_norm, area_threshold=200)
     cv2.imshow('Background Subtraction', fish)
     
     key = cv2.waitKey(1)
