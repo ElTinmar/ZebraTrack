9c2d6dfb5760c944b87888feb29bf5811e36da60
ElTinMar <martin.privat@hotmail.fr>
2023-04-27 14:36:11 +0200

    eye tracking

--- test/test_background.py
+++ test/test_background.py
@@ -3,16 +3,13 @@ import utils.background_model as bckg
 import cv2
 from utils.im2float import im2single
 from utils.im2gray import im2gray
-from utils.imadjust import imadjust
-from skimage.measure import regionprops, moments
-from scipy.ndimage import median_filter
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from math import pi
 import numpy as np
 from body.body_tracker import body_tracker_PCA
 from prey.prey_tracker import prey_tracker
-from utils.conncomp_filter import bwareafilter
+from eyes.eyes_tracker import eyes_tracker
 
 mov = OpenCV_VideoReader('toy_data/behavior_2000.avi',safe=False)
 
@@ -35,7 +32,7 @@ est_fish_area_pixel = int(pi * fish_length_mm/2 * fish_width_mm/2 * pixel_per_mm
 # Paramecia tracking
 
 alpha = 100
-threshold_intensity = 0.1
+threshold_intensity = 0.2
 threshold_area = est_fish_area_pixel
 
 threshold_intensity_eyes = 0.4
@@ -54,12 +51,11 @@ while True:
     bckg_sub = abs(im2single(im2gray(frame)) - background)
     
     # track fish body
-    centroid, component, _ = body_tracker_PCA(
+    fish_centroid, principal_components, _ = body_tracker_PCA(
         bckg_sub, 
         threshold_intensity, 
         threshold_area
     )
-    heading = component[:,0]
     
     # track paramecia
     prey_centroids, prey_mask = prey_tracker(
@@ -70,10 +66,13 @@ while True:
     )
 
     # track eyes
-    eye_mask = bwareafilter(
-        bckg_sub >= threshold_intensity_eyes, 
-        min_size=threshold_area_eye_min, 
-        max_size=threshold_area_eye_max
+    left_eye, right_eye, eye_mask = eyes_tracker(
+        bckg_sub,
+        threshold_intensity_eyes,
+        threshold_area_eye_min,
+        threshold_area_eye_max,
+        principal_components,
+        fish_centroid
     )
 
     # track tail
@@ -81,16 +80,61 @@ while True:
     # show tracking
     tracking = frame
 
-    if centroid is not None:
-        pt1 = centroid
-        pt2 = centroid + alpha*heading
+    # body tracking
+    if fish_centroid is not None:
+        pt1 = fish_centroid
+        pt2 = fish_centroid + alpha*principal_components[:,0]
         tracking = cv2.line(
             tracking,
             pt1.astype(np.int32),
             pt2.astype(np.int32),
             (0,0,255)
         )
+        pt1 = fish_centroid
+        pt2 = fish_centroid + alpha/2*principal_components[:,1]
+        tracking = cv2.line(
+            tracking,
+            pt1.astype(np.int32),
+            pt2.astype(np.int32),
+            (0,0,0)
+        )
 
+    # eye tracking
+    beta = 10
+    if left_eye is not None:
+        pt1 = left_eye.centroid
+        pt2 = pt1 + beta*left_eye.direction
+        tracking = cv2.line(
+            tracking,
+            pt1.astype(np.int32),
+            pt2.astype(np.int32),
+            (0,255,255)
+        )
+        pt2 = pt1 - beta*left_eye.direction
+        tracking = cv2.line(
+            tracking,
+            pt1.astype(np.int32),
+            pt2.astype(np.int32),
+            (0,255,255)
+        )
+    if right_eye is not None:
+        pt1 = right_eye.centroid
+        pt2 = pt1 + beta*right_eye.direction
+        tracking = cv2.line(
+            tracking,
+            pt1.astype(np.int32),
+            pt2.astype(np.int32),
+            (255,255,0)
+        )
+        pt2 = pt1 - beta*right_eye.direction
+        tracking = cv2.line(
+            tracking,
+            pt1.astype(np.int32),
+            pt2.astype(np.int32),
+            (255,255,0)
+        )
+
+    # prey tracking
     for prey_loc in prey_centroids:
         tracking = cv2.circle(tracking,prey_loc.astype(np.int32),10,(0,255,0))
 
