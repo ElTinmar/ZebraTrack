b91dfcd58f3dd92c18366c317404a1a0ed79b17e
ElTinmar <martin.privat@hotmail.fr>
2023-09-22 09:32:33 +0200

    moved stuff around

--- functional/utils/to_np_single_gray.py
+++
@@ -1,9 +0,0 @@
-import typing 
-from numpy.typing import NDArray
-import numpy as np
-
-def to_np_single_gray(frame: NDArray) -> NDArray[np.single]:
-    """
-    Convert an image from whatever bit depth to single precision grayscale
-    """
-    pass
