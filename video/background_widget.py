fdb45e648a18164954a12e847a5ed7088f1d004e
ElTinmar <martin.privat@hotmail.fr>
2023-10-13 12:55:37 +0200

    implementing

--- video/background_widget.py
+++ video/background_widget.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QPushButton, QLineEdit, QComboBox, QStackedWidget, QLabel, QVBoxLayout, QHBoxLayout, QWidget
 from video.background import BackgroundSubtractor, StaticBackground, DynamicBackground, DynamicBackgroundMP
 from video.video_reader import OpenCV_VideoReader
+from gui.custom_widgets.labeled_spinbox import LabeledSpinBox
 
 class BackgroundSubtractorWidget(QWidget):
     def __init__(self, *args, **kwargs):
@@ -14,16 +15,20 @@ class BackgroundSubtractorWidget(QWidget):
         self.layout_components()
 
     def declare_components(self):
-        # drop-down list to choose the background subtraction method
         self.static_filename = QLineEdit()
+        self.static_filename_label = QLabel()
+        self.static_filename_label.setText('Video file')
         folder_icon = QIcon.fromTheme('document-open')
         self.open_videofile_button = QPushButton()
         self.open_videofile_button.clicked.connect(self.open_file)
-        
+        self.static_numsamples = LabeledSpinBox()
+        self.static_numsamples.setText('Number images')
+
         self.parameters_static = QWidget()
         self.parameters_dynamic = QWidget()
         self.parameters_dynamic_mp = QWidget()
         
+        # drop-down list to choose the background subtraction method
         self.bckgsub_method_combobox = QComboBox(self)
         self.bckgsub_method_combobox.addItem('static')
         self.bckgsub_method_combobox.addItem('dynamic')
--- video/test/test_widget.py
+++ video/test/test_widget.py
@@ -2,8 +2,9 @@ import sys
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtWidgets import QApplication
 from video.background_widget import BackgroundSubtractorWidget 
+from gui.custom_widgets.labeled_editline_openfile import FileOpenLabeledEditButton
 
 app = QApplication(sys.argv)
-ex = BackgroundSubtractorWidget()
+ex = FileOpenLabeledEditButton()
 ex.show()
 sys.exit(app.exec_())
