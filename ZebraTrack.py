17adc0db8f952d9bb6ab4c2d67097acf4799b9c7
ElTinMar <martin.privat@hotmail.fr>
2023-10-26 17:42:17 +0200

    fixing import

--- ZebraTrack.py
+++ ZebraTrack.py
@@ -4,6 +4,6 @@ import numpy as np
 import sys
 import argparse
 from PyQt5.QtWidgets import QApplication
-from tracker.trackers import LinearSumAssignment, GridAssignment 
-from tracker.widgets import TrackerWidget, AnimalTrackerWidget, BodyTrackerWidget, EyesTrackerWidget,TailTrackerWidget
+from tracker import LinearSumAssignment, GridAssignment 
+from tracker import TrackerWidget, AnimalTrackerWidget, BodyTrackerWidget, EyesTrackerWidget,TailTrackerWidget
 
