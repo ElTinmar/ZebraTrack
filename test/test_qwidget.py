b937061c8a8893fe23be42fa6f4037ffb228c096
ElTinMar <martin.privat@hotmail.fr>
2023-10-27 17:03:12 +0200

    test roi selector

--- test/test_qwidget.py
+++ test/test_qwidget.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from tracker.assignment_widget import AssignmentWidget
+from tracker.image_tools.roi_selector_widget import ROISelectorWidget
+import numpy as np
+
+image = np.random.randint(0,255,(1024,1024),dtype=np.uint8) 
 
 app = QApplication(sys.argv)
-window = AssignmentWidget()
+window = ROISelectorWidget(image)
 window.show()
 sys.exit(app.exec())
 
