ef96b575459f1f6f7572b9d871e942eae432e3c8
ElTinMar <martin.privat@hotmail.fr>
2023-10-30 11:47:38 +0100

    test

--- test/test_qwidget.py
+++ test/test_qwidget.py
@@ -1,13 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from tracker.assignment_widget import AssignmentWidget
-from tracker.image_tools.roi_selector_widget import ROISelectorWidget
+from tracker.image_tools.roi_selector_widget import ROISelectorWidget, ROISelectorDialog
 import numpy as np
 
 image = np.random.randint(0,255,(1024,1024),dtype=np.uint8) 
 
 app = QApplication(sys.argv)
-window = ROISelectorWidget(image)
+window = ROISelectorDialog(image)
 window.show()
 sys.exit(app.exec())
 
