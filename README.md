8a5f5d70677529f79811381c3bec4cf0451ea091
ElTinMar <martin.privat@hotmail.fr>
2023-11-08 13:13:18 +0100

    package

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Download the repository and create new environment:
 
 
 ```
-git clone --recurse-submodules git@github.com:ElTinmar/ZebraTrack.git
+git clone git@github.com:ElTinmar/ZebraTrack.git
 cd ZebraTrack
 conda update conda
 conda install -c conda-forge mamba
--- gui.py
+++ gui.py
@@ -1,11 +1,9 @@
-from tracker import TrackerWidget
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTabWidget, QHBoxLayout
 from typing import List, Protocol
 from tracker import LinearSumAssignment, GridAssignment, TrackerWidget, AnimalTrackerWidget, BodyTrackerWidget, EyesTrackerWidget,TailTrackerWidget
-from tracker.image_tools import im2gray, im2single
-from tracker.video_tools import BackgroundSubtractorWidget
-from tracker.video_tools import PlaylistWidget
+from image_tools import im2gray, im2single
+from video_tools import BackgroundSubtractorWidget, PlaylistWidget
 
 # TODO add the possibility to dynamically add trackers in a new tab
 # TODO remove parameters from init, load everything from the GUI
